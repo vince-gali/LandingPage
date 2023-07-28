bbb1b1f3f95c5c83c3a6c367a3f189c297fd89a2
Vincent Galindo <vgalindo97@gmail.com>
2023-07-27 18:35:31 -0600

    eeeee

---
+++ LandingPage.client/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main class="bg-black">
+    <router-view />
+  </main>
+   <!-- <footer class="bg-dark text-light">
+    Made with 💖 by CodeWorks
+  </footer> -->
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root{
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
---
+++ LandingPage.client/src/pages/HomePage.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card p-5 bg-white rounded elevation-3">
+      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
+        class="rounded-circle">
+      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
+        Vue 3 Starter
+      </h1>
+    </div>
+  </div> -->
+  <section class="row">
+    <div class="text-center py-4 text-white">
+      <h5><em>Some kind of image here</em></h5>
+    </div>
+  </section>
+
+
+  <section class="row">
+    <div class="text-center py-4 text-white">
+      <h5><em>Project Highlights</em></h5>
+      <div class="card elevation-4  col-3 bg-white">
+        <img class="img-fluid" src="https://th.bing.com/th/id/R.49b026ec2df78cf2c7e584f083ce2042?rik=hoB8ydaEWq2T6Q&pid=ImgRaw&r=0" alt="">
+        <div class="display-flex align-items-center">
+          <p>project</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
